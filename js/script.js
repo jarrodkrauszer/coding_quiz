2165d942db0519187681f1c7366645f8ca0edbde
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-10 11:26:02 -0400

    worked on start quiz page

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const questions = (
+  {
+    question: 'Javascript is an _______ language?',
+    answers: ['object-oriented', 'object-based', 'procedural', 'none of the above'],
+    correctAnswerID: 0 
+  },
+  {
+    question: 'Commonly used data types do not include:',
+    answers: ['strings', 'booleans', 'alerts', 'numbers'],
+    correctAnswerID: 2 
+  },
+  {
+    question: 'Which of the following keywords is used to define a variable in Javascript?',
+    answers: ['var', 'let', 'both a and b', 'none of the above'],
+    correctAnswerID: 2 
+  },
+  {
+    question: 'Which of the following methods is used to access HTML elements using Javascript?',
+    answers: ['getElementByID()', 'getElementsByClassName()', 'both a and b', 'none of the above'],
+    correctAnswerID: 2 
+  },
+  {
+    question: 'Which of the following methods can be used to display data in some form using Javascript?',
+    answers: ['document.Write()', 'console.log()', 'window.alert()', 'all the above'],
+    correctAnswerID: 3 
+  }
+)
