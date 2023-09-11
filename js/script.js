cbe0dde65ac476deda2fb3530405f7520eb04fe4
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-11 19:45:38 -0400

    started coding game over page

--- js/script.js
+++ js/script.js
@@ -2,97 +2,137 @@ const questions = [
   {
     question: 'Javascript is an _______ language?',
     answers: ['object-oriented', 'object-based', 'procedural', 'none of the above'],
-    correctAnswerID: 0 
+    correctAnswer: 'object-oriented' 
   },
   {
     question: 'Commonly used data types do not include:',
     answers: ['strings', 'booleans', 'alerts', 'numbers'],
-    correctAnswerID: 2 
+    correctAnswer: 'alerts' 
   },
   {
     question: 'Which of the following keywords is used to define a variable in Javascript?',
     answers: ['var', 'let', 'both a and b', 'none of the above'],
-    correctAnswerID: 2 
+    correctAnswer: 'both a and b' 
   },
   {
     question: 'Which of the following methods is used to access HTML elements using Javascript?',
     answers: ['getElementByID()', 'getElementsByClassName()', 'both a and b', 'none of the above'],
-    correctAnswerID: 2 
+    correctAnswer: 'both a and b'
   },
   {
     question: 'You can use a text string as a literal value or assign it to a variable.',
     answers: ['true', 'false'],
-    correctAnswerID: 0 
+    correctAnswer: 'true'
   },
   {
     question: 'Which of the following methods can be used to display data in some form using Javascript?',
     answers: ['document.Write()', 'console.log()', 'window.alert()', 'all the above'],
-    correctAnswerID: 3 
+    correctAnswer: 'all the above' 
   }
 ];
 
 // Assignment Code
 var startBtn = document.querySelector("#start-quiz");
+var submitScoreBtn = document.querySelector('#submit-score');
 var startPage = document.querySelector("#start-page");
 var questionPage = document.querySelector("#question-page");
+var resultPage = document.querySelector('#result-popup')
+var gameOverPage = document.querySelector('#game-over');
 
-// Variables
+
+// Global Variables
 var numOfAnswers = 0;
 var score = 0;
-
-function displayQuestionPage() {
-  startPage.classList.add("hidden");
-  questionPage.classList.remove("hidden");
-}
+var lastPageDisplayed = 'start-page';
 
 function displayStartPage() {
-  startPage.classList.remove("hidden");
-  questionPage.classList.add("hidden");
+  startPage.classList.remove('hidden');
+  questionPage.classList.add('hidden');
+  gameOverPage.classList.add('hidden');
+  lastPageDisplayed = 'start-page';
 }
 
-function checkAnswer(buttonIndex) {
-  if(questions[numOfAnswers].correctAnswerID === buttonIndex) {
-    console.log('Correct!');
-    score += 1;
-  } else {
-    console.log('Wrong!'); 
-    score -= 1;
-  }
+function displayQuestionPage() {
+  questionPage.classList.remove('hidden');
+  startPage.classList.add('hidden');
+  gameOverPage.classList.add('hidden');
+  lastPageDisplayed = 'question-page';
+}
 
-  numOfAnswers++;
-  generateQuestion();
+function displayGameOverPage() {
+  gameOverPage.classList.remove('hidden');
+  startPage.classList.add('hidden');
+  questionPage.classList.add('hidden');
+  lastPageDisplayed = 'game-over';
+}
+
+function hideResultPopup() {
+  resultPage.classList.add("hidden");
 }
 
 function generateQuestion() {
   var answerBtnContainer = document.querySelector("#answer-buttons");
   answerBtnContainer.innerHTML = '';
 
-  if (numOfAnswers < questions.length) {
-    var newQuestion = document.querySelector("#question");
-    var questionIndex = 1;
-    newQuestion.innerText = questions[numOfAnswers].question;
+  var newQuestion = document.querySelector("#question");
+  var questionIndex = 1;
+  newQuestion.innerText = questions[numOfAnswers].question;
 
-    questions[numOfAnswers].answers.forEach((answer) => {
+  // Generates the answer buttons
+  questions[numOfAnswers].answers.forEach((answer) => {
+    var answerBtn = document.createElement('button');
+    answerBtn.id = `#answer-btn-${questionIndex}`;
+    answerBtn.classList.add('answer-btn');
+    // answerBtn.setAttribute('id', `#answer-btn-${questionIndex}`);
+    // answerBtn.setAttribute('class', 'answer-btn');
+    var text = document.createTextNode(`${questionIndex}. ${answer}`);
+    answerBtn.appendChild(text);
+    answerBtnContainer.appendChild(answerBtn);
 
-      // let answerBtn = document.createElement('button', {
-      //   id: `#answer-btn-${questionIndex}`,
-      //   class: 'answer-btn'
-      // })
+    answerBtn.addEventListener("click", answerClicked);
+    questionIndex++;
 
-      let answerBtn = document.createElement('button');
-      answerBtn.setAttribute('id', `#answer-btn-${questionIndex}`);
-      answerBtn.setAttribute('class', 'answer-btn');
+  }); 
 
-      answerBtn.innerText = `${questionIndex}. ${answer}`;
-      answerBtnContainer.appendChild(answerBtn);
-      answerBtn.addEventListener("click", answerClicked);
-      questionIndex++;
+}
 
-    });
-    
+function gameOver () {
+  displayGameOverPage();
+ 
+  var finalScore = document.querySelector('#final-score-text');
+  finalScore.innerText = `You final score is ${score}.`;
+
+  numOfAnswers = 0;
+}
+
+function checkAnswer(answerIndex) {
+  var resultText = document.querySelector('#result-text');
+  var result = '';
+  
+  if(questions[numOfAnswers].answers[answerIndex] === questions[numOfAnswers].correctAnswer) {
+    result = 'Correct!';
+    score++;
   } else {
-    console.log('Game Over!  Your score is: ' + score);
+    result = 'Wrong!'; 
+    score--;
   }
+
+  resultText.innerText = result;
+  resultPage.classList.remove('hidden');
+  setTimeout(hideResultPopup, 1500);
+
+  numOfAnswers++;
+  if (numOfAnswers < questions.length) {
+    generateQuestion();
+  } else {
+    setTimeout(gameOver, 1000);
+  }
+}
+
+function submitScore() {
+  var initials = document.querySelector('#initials');
+
+  console.log('Initials: ' + initials.value);
 }
 
 function startQuiz() {
@@ -100,6 +140,7 @@ function startQuiz() {
   generateQuestion();
   displayQuestionPage();
 }
+
 // Button Click Event for the answer buttons  
 const answerClicked = (event) => {
   var buttonPressed = parseInt(event.target.id.slice(-1)) ;
@@ -108,3 +149,4 @@ const answerClicked = (event) => {
 
 // Add event listener to generate button
 startBtn.addEventListener("click", startQuiz);
+submitScoreBtn.addEventListener("click", submitScore);
