6518d13015475ac2363a00e9cb6e8e0e8f96a8c2
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-11 13:15:24 -0400

    worked on creating the answer buttons in js instead of hard coded in the HTML

--- js/script.js
+++ js/script.js
@@ -20,6 +20,11 @@ const questions = [
     correctAnswerID: 2 
   },
   {
+    question: 'You can use a text string as a literal value or assign it to a variable.',
+    answers: ['true', 'false'],
+    correctAnswerID: 0 
+  },
+  {
     question: 'Which of the following methods can be used to display data in some form using Javascript?',
     answers: ['document.Write()', 'console.log()', 'window.alert()', 'all the above'],
     correctAnswerID: 3 
@@ -30,24 +35,28 @@ const questions = [
 var startBtn = document.querySelector("#start-quiz");
 var startPage = document.querySelector("#start-page");
 var questionPage = document.querySelector("#question-page");
-// var answerBtn1 = document.querySelector("#answer-btn-1");
-// var answerBtn2 = document.querySelector("#answer-btn-2");
-// var answerBtn3 = document.querySelector("#answer-btn-3");
-// var answerBtn4 = document.querySelector("#answer-btn-4");
 
 // Variables
 var numOfAnswers = 0;
-var gameOver = true;
 var score = 0;
 
-function checkAnswer(buttonIndex) {
+function displayQuestionPage() {
+  startPage.classList.add("hidden");
+  questionPage.classList.remove("hidden");
+}
 
+function displayStartPage() {
+  startPage.classList.remove("hidden");
+  questionPage.classList.add("hidden");
+}
+
+function checkAnswer(buttonIndex) {
   if(questions[numOfAnswers].correctAnswerID === buttonIndex) {
     console.log('Correct!');
-    score += 5;
+    score += 1;
   } else {
     console.log('Wrong!'); 
-    score -= 2;
+    score -= 1;
   }
 
   numOfAnswers++;
@@ -55,7 +64,7 @@ function checkAnswer(buttonIndex) {
 }
 
 function generateQuestion() {
-  var answerBtnContainer = document.querySelector("#answer-container");
+  var answerBtnContainer = document.querySelector("#answer-buttons");
   answerBtnContainer.innerHTML = '';
 
   if (numOfAnswers < questions.length) {
@@ -77,37 +86,21 @@ function generateQuestion() {
       answerBtn.innerText = `${questionIndex}. ${answer}`;
       answerBtnContainer.appendChild(answerBtn);
       answerBtn.addEventListener("click", answerClicked);
+      questionIndex++;
 
     });
-
-    // questions[numOfAnswers].answers.forEach((answer) => {
-    //   var answerBtn = document.querySelector(`#answer-btn-${questionIndex}`)
-    //   answerBtn.innerText = `${questionIndex}. ${answer}`
-    //   questionIndex++;
-    // });
     
   } else {
-    console.log('Game Over!');
+    console.log('Game Over!  Your score is: ' + score);
   }
 }
 
-function displayQuestionPage() {
-  startPage.classList.add("hidden");
-  questionPage.classList.remove("hidden");
-}
-
-function displayStartPage() {
-  startPage.classList.remove("hidden");
-  questionPage.classList.add("hidden");
-}
-
 function startQuiz() {
-  gameOver = false;
+  numOfAnswers = 0;
   generateQuestion();
   displayQuestionPage();
-
 }
-
+// Button Click Event for the answer buttons  
 const answerClicked = (event) => {
   var buttonPressed = parseInt(event.target.id.slice(-1)) ;
   checkAnswer(buttonPressed - 1);
@@ -115,7 +108,3 @@ const answerClicked = (event) => {
 
 // Add event listener to generate button
 startBtn.addEventListener("click", startQuiz);
-// answerBtn1.addEventListener("click", answerClicked);
-// answerBtn2.addEventListener("click", answerClicked);
-// answerBtn3.addEventListener("click", answerClicked);
-// answerBtn4.addEventListener("click", answerClicked);
