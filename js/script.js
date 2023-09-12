dd9083cc329007a50853a77df90fd5d8328c0dcb
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-12 17:22:18 -0400

    worked on getting high scores saved to local storage

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ var startPage = document.querySelector("#start-page");
 var questionPage = document.querySelector("#question-page");
 var resultPage = document.querySelector('#result-popup')
 var gameOverPage = document.querySelector('#game-over');
+var timeLeft = document.querySelector('#timer');
 
 // Global Variables
 var numOfAnswers = 0;
 var score = 0;
 var gameTimer = 0;
+var timerInterval;
 
 function displayStartPage() {
   startPage.classList.remove('hidden');
@@ -76,6 +78,11 @@ function checkAnswer(answer) {
     score++;
   } else {
     result = 'Wrong!'; 
+    gameTimer -= 10;
+  }
+
+  if (gameTimer < 0) {
+    gameTimer = 0;
   }
 
   resultText.innerText = result;
@@ -86,7 +93,9 @@ function checkAnswer(answer) {
 
   if (gameTimer === 0 || numOfAnswers === questions.length) {
     setTimeout(gameOver, 1000);
-
+    gameTimer = 0;
+    timeLeft.textContent = gameTimer;
+    clearInterval(timerInterval);
   } else {
     generateQuestion();
   }
@@ -94,13 +103,68 @@ function checkAnswer(answer) {
 
 function submitScore() {
   var initials = document.querySelector('#initials');
+  
+  var player = {
+    name: initials.value,
+    highScore: score
+  };
 
-  console.log('Initials: ' + initials.value);
+  addPlayerToStorage(player);
+  displayStartPage();
+}
+
+function addPlayerToStorage(player) {
+  const playersFromStorage = getPlayersFromStorage();
+  
+  if (checkIfPlayerExists) {
+
+  } else {
+    
+  }
+  // Add new item to array
+  playersFromStorage.push(player);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('player', JSON.stringify(playersFromStorage));
+}
+
+function getPlayersFromStorage() {
+  let playersFromStorage;
+
+  if (localStorage.getItem('player') === null) {
+    playersFromStorage = [];
+  } else {
+    playersFromStorage = JSON.parse(localStorage.getItem('player'));
+  }
+  console.log(playersFromStorage);
+  return playersFromStorage;
+}
+
+function checkIfPlayerExists(player) {
+  const playersFromStorage = getItemsFromStorage();
+  return playersFromStorage.includes(player);
+}
+
+function setTime() {
+  // Sets interval in variable
+  timerInterval = setInterval(function() {
+    gameTimer--;
+    timeLeft.textContent = gameTimer;
+
+    if(gameTimer === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      gameOver();
+    }
+
+  }, 1000);
 }
 
 function startQuiz() {
   numOfAnswers = 0;
-  gameTimer = 120;
+  gameTimer = 60;
+  setTime();
   generateQuestion();
   displayQuestionPage();
 }
